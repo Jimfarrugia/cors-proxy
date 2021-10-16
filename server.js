6948045418bfd1abcba64f8b5004ac123f889246
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-17 10:38:43 +1100

    Use port 5000 locally or env var if available

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const corsAnywhere = require("cors-anywhere");
-const CORS_PROXY_PORT = 5000;
+const port = process.env.PORT || 5000;
 
 // Create CORS Anywhere server
 corsAnywhere
   .createServer({})
-  .listen(CORS_PROXY_PORT, () =>
+  .listen(port, () =>
     console.log(`CORS Anywhere server started at port ${CORS_PROXY_PORT}`)
   );
