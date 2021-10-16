a8ef249643425c927bf5381e97ff63b4043f663b
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-17 10:40:17 +1100

    Fix typo

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 5000;
 corsAnywhere
   .createServer({})
   .listen(port, () =>
-    console.log(`CORS Anywhere server started at port ${CORS_PROXY_PORT}`)
+    console.log(`CORS Anywhere server started at port ${port}`)
   );
