9f838d1a008da80134208b3619ab90713a5507aa
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-18 06:15:03 +1100

    Add origin whitelist

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const corsAnywhere = require("cors-anywhere");
 const port = process.env.PORT || 5000;
 
-// Create CORS Anywhere server
 corsAnywhere
-  .createServer({})
+  .createServer({
+    originWhitelist: ["https://react-crypto-watcher.netlify.app"],
+  })
   .listen(port, () =>
     console.log(`CORS Anywhere server started at port ${port}`)
   );
