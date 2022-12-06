88d116a4568469ebc5e047f1e956056a9bf4304b
jimfarrugia <the.jim.farrugia@gmail.com>
2022-12-07 05:46:14 +1100

    update whitelist (localhost)

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 5000;
 
 corsAnywhere
   .createServer({
-    originWhitelist: ["https://crypto-watch.netlify.app"],
+    originWhitelist: ["https://crypto-watch.netlify.app", "http://localhost:3000"],
   })
   .listen(port, () =>
     console.log(`CORS Anywhere server started at port ${port}`)
